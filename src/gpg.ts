9b00559503b0676fa34d9ef5386920f6feb39cc7
Junfeng Zhang <jeffzx15o@gmail.com>
2022-11-09 20:15:12 +0800

    fix: gpg

--- src/gpg.ts
+++ src/gpg.ts
@@ -1,41 +1,43 @@
+import * as core from "@actions/core";
 import * as exec from "@actions/exec";
 import * as tc from "@actions/tool-cache";
 import { ExecOptions } from "@actions/exec/lib/interfaces";
 
 export async function importKeys() {
-  await setupKeys();
-}
-
-export async function setupKeys() {
   let keys = await tc.downloadTool("https://swift.org/keys/all-keys.asc");
-
-  let output = "";
 
   const options: ExecOptions = {
     silent: true,
     listeners: {
       stdout: (data: Buffer) => {
-        output += data.toString();
+        core.info(data.toString().trim());
+      },
+      stderr: (data: Buffer) => {
+        core.error(data.toString().trim());
+      },
+    },
+  };
+
+  await exec.exec("gpg", ["--import", keys], options);
+}
+
+export async function verify(signature: string, archive: string) {
+  const options: ExecOptions = {
+    silent: true,
+    listeners: {
+      stdout: (data: Buffer) => {
+        core.info(data.toString().trim());
+      },
+      stderr: (data: Buffer) => {
+        core.error(data.toString().trim());
       },
     },
   };
 
   await exec.exec(
     "gpg",
-    ["--batch", "--import-options", "import-show", "--import", keys],
+    ["--keyserver", "hkp://keyserver.ubuntu.com", "--refresh-keys", "Swift"],
     options
   );
-
-  await exec.exec("gpg", [
-    "--batch",
-    "--quiet",
-    "--keyserver",
-    "keyserver.ubuntu.com",
-    "--recv-keys",
-    keys,
-  ]);
-}
-
-export async function verify(signature: string, archive: string) {
-  await exec.exec("gpg", ["--batch", "--verify", signature, archive]);
+  await exec.exec("gpg", ["--verify", signature, archive]);
 }
