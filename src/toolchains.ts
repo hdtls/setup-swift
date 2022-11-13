83a93886cdd1daad49bb438523dd53883cd0f7ae
Junfeng Zhang <ph.gitio@gmail.com>
2022-11-13 23:39:15 +0800

    feat: toolchains utils

---
+++ src/toolchains.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import * as pl from 'plist';
+
+export function parseBundleIDFromDirectory(at: string): string {
+  try {
+    const { CFBundleIdentifier, Identifier } = pl.parse(
+      fs.readFileSync(path.join(at, 'Info.plist'), 'utf8')
+    ) as {
+      CFBundleIdentifier?: string;
+      Identifier?: string;
+    };
+
+    return CFBundleIdentifier || Identifier || '';
+  } catch (error) {
+    return '';
+  }
+}
+
+export function getToolchain(named: string): string {
+  return path.join(
+    os.homedir(),
+    '/Library/Developer/Toolchains',
+    `${named}.xctoolchain`
+  );
+}
+
+export function getXcodeDefaultToolchain(): string {
+  return '/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain';
+}
