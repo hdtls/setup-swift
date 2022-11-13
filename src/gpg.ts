d7ec69f1752cfceca2abe0881c0738f342404d5f
Junfeng Zhang <ph.gitio@gmail.com>
2022-11-14 00:31:14 +0800

    style: prettierrc

--- src/gpg.ts
+++ src/gpg.ts
@@ -1,20 +1,19 @@
-import * as core from "@actions/core";
-import * as exec from "@actions/exec";
-import * as tc from "@actions/tool-cache";
+import * as exec from '@actions/exec';
+import * as tc from '@actions/tool-cache';
 
 export async function importKeys() {
-  let keys = await tc.downloadTool("https://swift.org/keys/all-keys.asc");
+  let keys = await tc.downloadTool('https://swift.org/keys/all-keys.asc');
 
-  await exec.exec("gpg", ["--import", keys]);
+  await exec.exec('gpg', ['--import', keys]);
 }
 
 export async function verify(signature: string, archive: string) {
-  await exec.exec("gpg", [
-    "--keyserver",
-    "hkp://keyserver.ubuntu.com",
-    "--refresh-keys",
-    "Swift",
+  await exec.exec('gpg', [
+    '--keyserver',
+    'hkp://keyserver.ubuntu.com',
+    '--refresh-keys',
+    'Swift'
   ]);
 
-  await exec.exec("gpg", ["--verify", signature, archive]);
+  await exec.exec('gpg', ['--verify', signature, archive]);
 }
