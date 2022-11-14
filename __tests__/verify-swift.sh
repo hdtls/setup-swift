a7c2fd83982702ac91949ce64075fe005b915518
Junfeng Zhang <ph.gitio@gmail.com>
2022-11-15 00:46:20 +0800

    fix: swift version regex error

--- __tests__/verify-swift.sh
+++ __tests__/verify-swift.sh
@@ -6,25 +6,30 @@ if [ -z "$1" ]; then
 fi
 
 parse_expect_version() {
-  if [[ "$1" =~ ^[0-9]\.[0-9](\.[0-9])?$ ]]; then
+  if [[ "$1" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?(-dev)?$ ]]; then
     expected="$1"
-  fi
-
-  if [[ "$1" =~ ^[0-9]\.[0-9](\/[0-9])?-dev$ ]]; then
-    expected="$1"
-  fi
-
-  if [[ "$1" =~ ^swift-([0-9]\.[0-9](\.[0-9])?)-RELEASE$ ]]; then
+  elif [[ "$1" =~ ^swift-([0-9]+\.[0-9]+(\.[0-9]+)?)-RELEASE$ ]]; then
     expected="${BASH_REMATCH[1]}"
-  fi
+  
+  # The version number of DEVELOPMENT-SNAPSHOT is ambiguous
+  # there if not contains PATCH number we will use a RegEx test instead.
+  elif [[ "$1" =~ ^swift-([0-9])+\.([0-9])+(\.([0-9])+)?-DEVELOPMENT-SNAPSHOT ]]; then
+    local MAJOR="${BASH_REMATCH[1]}"
+    local MINOR="${BASH_REMATCH[2]}"
+    local PATCH="${BASH_REMATCH[4]}"
 
-  if [[ "$1" =~ ^swift-([0-9]\.[0-9](\.[0-9])?)-DEVELOPMENT-SNAPSHOT ]]; then
-    expected="${BASH_REMATCH[1]}-dev"
+    if [[ -n "$PATCH" ]]; then
+      expected="$MAJOR.$MINOR.$PATCH-dev"
+    else
+    expected="$MAJOR.$MINOR(\.[0-9]+)?-dev"
+    fi
+  else
+    exit 1
   fi
 }
 
 parse_version_from_log() {
-  if [[ "$1" =~ ([0-9]\.[0-9](\.[0-9])?(-dev)?) ]]; then
+  if [[ "$1" =~ ([0-9]+\.[0-9]+(\.[0-9]+)?(-dev)?) ]]; then
     swift_version="${BASH_REMATCH[1]}"
   fi
 }
@@ -32,19 +37,19 @@ parse_version_from_log() {
 parse_expect_version "$1"
 parse_version_from_log "$(swift --version)"
 
-if [[ $swift_version != $expected ]]; then
-  echo "AssertEqual 'swift --version' and $expected failed: $swift_version is not equal to $expected ("$1")"
-  exit 1
+if [[ $swift_version =~ $expected ]]; then
+  echo "Assert RegEx Match 'swift --version ($swift_version)' and $expected ("$1") success"
 else
-  echo "AssertEqual 'swift --version' and $expected ("$1") success"
+  echo "Assert RegEx Match 'swift --version ($swift_version)' and $expected failed: $swift_version is not match to $expected ("$1")"
+  exit 1
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   parse_version_from_log "$(xcrun swift --version)"
-  if [[ $swift_version != $expected ]]; then
-    echo "AssertEqual 'xcrun swift --version' and $expected failed: $swift_version is not equal to $expected ("$1")"
-    exit 1
+  if [[ $swift_version =~ $expected ]]; then
+    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $expected ("$1") success"
   else
-    echo "AssertEqual 'xcrun swift --version' and $expected ("$1") success"
+    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $expected failed: $swift_version is not match to $expected ("$1")"
+    exit 1
   fi
 fi
--- src/manifest.ts
+++ src/manifest.ts
@@ -9,30 +9,29 @@ export function resolve(
   let SWIFT_VERSION = versionSpec;
   let SWIFT_BRANCH = '';
 
-  let RE = /^[\d]\.[\d](\.[\d])?$/;
+  let re = /^\d+\.\d+(\.\d+)?$/;
   let hasMatch = false;
-  if (RE.test(versionSpec)) {
+  if (re.test(versionSpec)) {
     SWIFT_BRANCH = `swift-${versionSpec}-release`;
     SWIFT_VERSION = `swift-${versionSpec}-RELEASE`;
     hasMatch = true;
   }
 
-  RE = /^swift-DEVELOPMENT-.+-a$/;
-  if (RE.test(versionSpec) && !hasMatch) {
+  re = /^swift-DEVELOPMENT-.+-a$/;
+  if (re.test(versionSpec) && !hasMatch) {
     SWIFT_BRANCH = 'development';
     hasMatch = true;
   }
 
-  RE = /^swift-[\d]\.[\d](\.[\d])?-RELEASE$/;
-  if (RE.test(versionSpec) && !hasMatch) {
+  re = /^swift-\d+\.\d+(\.\d+)?-RELEASE$/;
+  if (re.test(versionSpec) && !hasMatch) {
     SWIFT_BRANCH = versionSpec.toLowerCase();
     hasMatch = true;
   }
 
-  RE = /^swift-(?<version>[\d]\.[\d](\.[\d])?)-DEVELOPMENT-.+-a$/;
-  if (RE.test(versionSpec) && !hasMatch) {
-    const matched = versionSpec.match(RE);
-    SWIFT_BRANCH = `swift-${matched!.groups!.version}-branch`;
+  re = /^swift-(\d+\.\d+(\.\d+)?)-DEVELOPMENT-.+-a$/;
+  if (re.test(versionSpec) && !hasMatch) {
+    SWIFT_BRANCH = `swift-${versionSpec.replace(re, '$1')}-branch`;
     hasMatch = true;
   }
 
@@ -64,7 +63,7 @@ export function resolve(
 
   return {
     version: SWIFT_VERSION,
-    stable: /^swift-[\d]\.[\d](\.[\d])?-RELEASE$/.test(SWIFT_VERSION),
+    stable: /^swift-\d+\.\d+(\.\d+)?-RELEASE$/.test(SWIFT_VERSION),
     release_url: '',
     files: [
       {
