ac57f47acafec5093347e9868147066c426e5de2
Junfeng Zhang <ph.gitio@gmail.com>
2022-12-07 18:12:09 +0800

    feat: common regular expressions

---
+++ src/re.ts
@@ -0,0 +1,27 @@
+/**
+ * Semantic version RegExp
+ */
+export const SWIFT_SEMANTIC_VERSION = /^\d+.\d+(.\d+)?$/;
+
+/**
+ * Swift release RegExp
+ */
+export const SWIFT_RELEASE = /^swift-(\d+.\d+(.\d+)?)-RELEASE$/;
+
+/**
+ * Swift nightly(development snapshot) RegExp
+ */
+export const SWIFT_NIGHTLY =
+  /^(swift|nightly)-(\d+.\d+)(-DEVELOPMENT-SNAPSHOT-.+-a)?$/;
+
+/**
+ * Swift nightly(mainline development snapshot) RegExp
+ */
+export const SWIFT_MAINLINE_NIGHTLY =
+  /^(nightly(-main)?|swift-DEVELOPMENT-SNAPSHOT-.+-a)$/;
+
+/**
+ * Swift TOOLCHAINS RegExp
+ */
+export const TOOLCHAINS =
+  /(CFBundle)?Identifier<\/key>\n*\t*<string>(?<TOOLCHAINS>.*)<\/string>/;
