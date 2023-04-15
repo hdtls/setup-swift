e701aa62632d4f9296e6e60c74688fa7da942678
Junfeng Zhang <jeffzx15o@gmail.com>
2023-04-16 02:40:00 +0800

    feat: allow find default installed swift on linux

--- dist/setup/index.js
+++ dist/setup/index.js
@@ -3043,7 +3043,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports._readLinuxVersionFile = exports._getOsVersion = exports._findMatch = void 0;
-const semver = __importStar(__nccwpck_require__(5911));
+const semver = __importStar(__nccwpck_require__(562));
 const core_1 = __nccwpck_require__(2186);
 // needs to be require for core node modules to be mocked
 /* eslint @typescript-eslint/no-require-imports: 0 */
@@ -3278,7 +3278,7 @@ const mm = __importStar(__nccwpck_require__(2473));
 const os = __importStar(__nccwpck_require__(2037));
 const path = __importStar(__nccwpck_require__(1017));
 const httpm = __importStar(__nccwpck_require__(6255));
-const semver = __importStar(__nccwpck_require__(5911));
+const semver = __importStar(__nccwpck_require__(562));
 const stream = __importStar(__nccwpck_require__(2781));
 const util = __importStar(__nccwpck_require__(3837));
 const assert_1 = __nccwpck_require__(9491);
@@ -3905,91 +3905,7 @@ function _unique(values) {
 
 /***/ }),
 
-/***/ 7701:
-/***/ ((module) => {
-
-/**
- * Convert array of 16 byte values to UUID string format of the form:
- * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
- */
-var byteToHex = [];
-for (var i = 0; i < 256; ++i) {
-  byteToHex[i] = (i + 0x100).toString(16).substr(1);
-}
-
-function bytesToUuid(buf, offset) {
-  var i = offset || 0;
-  var bth = byteToHex;
-  // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
-  return ([
-    bth[buf[i++]], bth[buf[i++]],
-    bth[buf[i++]], bth[buf[i++]], '-',
-    bth[buf[i++]], bth[buf[i++]], '-',
-    bth[buf[i++]], bth[buf[i++]], '-',
-    bth[buf[i++]], bth[buf[i++]], '-',
-    bth[buf[i++]], bth[buf[i++]],
-    bth[buf[i++]], bth[buf[i++]],
-    bth[buf[i++]], bth[buf[i++]]
-  ]).join('');
-}
-
-module.exports = bytesToUuid;
-
-
-/***/ }),
-
-/***/ 7269:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-// Unique ID creation requires a high quality random # generator.  In node.js
-// this is pretty straight-forward - we use the crypto API.
-
-var crypto = __nccwpck_require__(6113);
-
-module.exports = function nodeRNG() {
-  return crypto.randomBytes(16);
-};
-
-
-/***/ }),
-
-/***/ 7468:
-/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
-
-var rng = __nccwpck_require__(7269);
-var bytesToUuid = __nccwpck_require__(7701);
-
-function v4(options, buf, offset) {
-  var i = buf && offset || 0;
-
-  if (typeof(options) == 'string') {
-    buf = options === 'binary' ? new Array(16) : null;
-    options = null;
-  }
-  options = options || {};
-
-  var rnds = options.random || (options.rng || rng)();
-
-  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
-  rnds[6] = (rnds[6] & 0x0f) | 0x40;
-  rnds[8] = (rnds[8] & 0x3f) | 0x80;
-
-  // Copy bytes to buffer, if provided
-  if (buf) {
-    for (var ii = 0; ii < 16; ++ii) {
-      buf[i + ii] = rnds[ii];
-    }
-  }
-
-  return buf || bytesToUuid(rnds);
-}
-
-module.exports = v4;
-
-
-/***/ }),
-
-/***/ 5911:
+/***/ 562:
 /***/ ((module, exports) => {
 
 exports = module.exports = SemVer
@@ -5592,6 +5508,90 @@ function coerce (version, options) {
 
 /***/ }),
 
+/***/ 7701:
+/***/ ((module) => {
+
+/**
+ * Convert array of 16 byte values to UUID string format of the form:
+ * XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+ */
+var byteToHex = [];
+for (var i = 0; i < 256; ++i) {
+  byteToHex[i] = (i + 0x100).toString(16).substr(1);
+}
+
+function bytesToUuid(buf, offset) {
+  var i = offset || 0;
+  var bth = byteToHex;
+  // join used to fix memory issue caused by concatenation: https://bugs.chromium.org/p/v8/issues/detail?id=3175#c4
+  return ([
+    bth[buf[i++]], bth[buf[i++]],
+    bth[buf[i++]], bth[buf[i++]], '-',
+    bth[buf[i++]], bth[buf[i++]], '-',
+    bth[buf[i++]], bth[buf[i++]], '-',
+    bth[buf[i++]], bth[buf[i++]], '-',
+    bth[buf[i++]], bth[buf[i++]],
+    bth[buf[i++]], bth[buf[i++]],
+    bth[buf[i++]], bth[buf[i++]]
+  ]).join('');
+}
+
+module.exports = bytesToUuid;
+
+
+/***/ }),
+
+/***/ 7269:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+// Unique ID creation requires a high quality random # generator.  In node.js
+// this is pretty straight-forward - we use the crypto API.
+
+var crypto = __nccwpck_require__(6113);
+
+module.exports = function nodeRNG() {
+  return crypto.randomBytes(16);
+};
+
+
+/***/ }),
+
+/***/ 7468:
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+var rng = __nccwpck_require__(7269);
+var bytesToUuid = __nccwpck_require__(7701);
+
+function v4(options, buf, offset) {
+  var i = buf && offset || 0;
+
+  if (typeof(options) == 'string') {
+    buf = options === 'binary' ? new Array(16) : null;
+    options = null;
+  }
+  options = options || {};
+
+  var rnds = options.random || (options.rng || rng)();
+
+  // Per 4.4, set bits for version and `clock_seq_hi_and_reserved`
+  rnds[6] = (rnds[6] & 0x0f) | 0x40;
+  rnds[8] = (rnds[8] & 0x3f) | 0x80;
+
+  // Copy bytes to buffer, if provided
+  if (buf) {
+    for (var ii = 0; ii < 16; ++ii) {
+      buf[i + ii] = rnds[ii];
+    }
+  }
+
+  return buf || bytesToUuid(rnds);
+}
+
+module.exports = v4;
+
+
+/***/ }),
+
 /***/ 4294:
 /***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
 
@@ -6574,16 +6574,37 @@ function find(manifest) {
         if (toolPath) {
             return toolPath;
         }
-        // Find Default Xocde toolchain if swift version equals to requested
-        // we should avoid install action just return Xcode default toolchain as toolPath.
-        if (process.platform == 'darwin' && re.SWIFT_RELEASE.test(manifest.version)) {
-            const commandLine = path_1.default.join(toolchains.getXcodeDefaultToolchain(), '/usr/bin/swift');
-            if (fs.existsSync(commandLine)) {
-                const { stdout } = yield exec.getExecOutput(commandLine, ['--version']);
-                if (utils.getVersion(stdout) ==
-                    manifest.version.replace(re.SWIFT_RELEASE, '$1')) {
-                    return toolchains.getXcodeDefaultToolchain();
-                }
+        // If required version is nightly version just return not found.
+        if (re.SWIFT_NIGHTLY.test(manifest.version) ||
+            re.SWIFT_MAINLINE_NIGHTLY.test(manifest.version)) {
+            core.info(`Version ${manifest.version} was not found in the local cache`);
+            return '';
+        }
+        let commandLine = '';
+        switch (manifest.files[0].platform) {
+            case 'darwin':
+                // Find Default Xocde toolchain if swift version equals to requested
+                // we should avoid install action and return Xcode default toolchain as toolPath.
+                commandLine = path_1.default.join(toolchains.getXcodeDefaultToolchain(), '/usr/bin/swift');
+                toolPath = toolchains.getXcodeDefaultToolchain();
+                break;
+            case 'ubuntu':
+                commandLine = '/usr/bin/swift';
+                toolPath = '/';
+                break;
+            case 'centos':
+            case 'amazonlinux':
+                commandLine = '/usr/lib/swift';
+                toolPath = '/';
+                break;
+            default:
+                break;
+        }
+        if (fs.existsSync(commandLine)) {
+            const { stdout } = yield exec.getExecOutput(commandLine, ['--version']);
+            if (utils.getVersion(stdout) ==
+                manifest.version.replace(re.SWIFT_RELEASE, '$1')) {
+                return toolPath;
             }
         }
         core.info(`Version ${manifest.version} was not found in the local cache`);
--- src/finder.ts
+++ src/finder.ts
@@ -14,21 +14,46 @@ export async function find(manifest: tc.IToolRelease) {
     return toolPath;
   }
 
-  // Find Default Xocde toolchain if swift version equals to requested
-  // we should avoid install action just return Xcode default toolchain as toolPath.
-  if (process.platform == 'darwin' && re.SWIFT_RELEASE.test(manifest.version)) {
-    const commandLine = path.join(
-      toolchains.getXcodeDefaultToolchain(),
-      '/usr/bin/swift'
-    );
-    if (fs.existsSync(commandLine)) {
-      const { stdout } = await exec.getExecOutput(commandLine, ['--version']);
-      if (
-        utils.getVersion(stdout) ==
-        manifest.version.replace(re.SWIFT_RELEASE, '$1')
-      ) {
-        return toolchains.getXcodeDefaultToolchain();
-      }
+  // If required version is nightly version just return not found.
+  if (
+    re.SWIFT_NIGHTLY.test(manifest.version) ||
+    re.SWIFT_MAINLINE_NIGHTLY.test(manifest.version)
+  ) {
+    core.info(`Version ${manifest.version} was not found in the local cache`);
+    return '';
+  }
+
+  let commandLine = '';
+  switch (manifest.files[0].platform) {
+    case 'darwin':
+      // Find Default Xocde toolchain if swift version equals to requested
+      // we should avoid install action and return Xcode default toolchain as toolPath.
+      commandLine = path.join(
+        toolchains.getXcodeDefaultToolchain(),
+        '/usr/bin/swift'
+      );
+      toolPath = toolchains.getXcodeDefaultToolchain();
+      break;
+    case 'ubuntu':
+      commandLine = '/usr/bin/swift';
+      toolPath = '/';
+      break;
+    case 'centos':
+    case 'amazonlinux':
+      commandLine = '/usr/lib/swift';
+      toolPath = '/';
+      break;
+    default:
+      break;
+  }
+
+  if (fs.existsSync(commandLine)) {
+    const { stdout } = await exec.getExecOutput(commandLine, ['--version']);
+    if (
+      utils.getVersion(stdout) ==
+      manifest.version.replace(re.SWIFT_RELEASE, '$1')
+    ) {
+      return toolPath;
     }
   }
 
