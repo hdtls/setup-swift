a7ff28774dfa4986ee9f01ffddd4f21438eb98ee
Junfeng Zhang <jeffzx15o@gmail.com>
2023-04-17 00:36:42 +0800

    fix: semver regex capture pattern error

--- __tests__/verify-swift.sh
+++ __tests__/verify-swift.sh
@@ -6,14 +6,14 @@ if [ -z "$1" ]; then
 fi
 
 parse_expect_version() {
-  if [[ "$1" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?(-dev)?$ ]]; then
+  if [[ "$1" =~ ^[0-9]+\.[0-9]+(\.[0-9]+)?$ ]]; then
     expected="$1"
   elif [[ "$1" =~ ^swift-([0-9]+\.[0-9]+(\.[0-9]+)?)-RELEASE$ ]]; then
     expected="${BASH_REMATCH[1]}"
   
   # The version number of DEVELOPMENT-SNAPSHOT is ambiguous
   # there if not contains PATCH number we will use a RegEx test instead.
-  elif [[ "$1" =~ ^swift-([0-9])+\.([0-9])+(\.([0-9])+)?-DEVELOPMENT-SNAPSHOT ]] || [[ "$1" =~ ^nightly-([0-9])+\.([0-9])+(\.([0-9])+)? ]]; then
+  elif [[ "$1" =~ ^swift-([0-9]+)\.([0-9]+)(\.([0-9]+))?-DEVELOPMENT-SNAPSHOT ]] || [[ "$1" =~ ^nightly-([0-9]+)\.([0-9]+)(\.([0-9]+))? ]]; then
     local MAJOR="${BASH_REMATCH[1]}"
     local MINOR="${BASH_REMATCH[2]}"
     local PATCH="${BASH_REMATCH[4]}"
@@ -38,18 +38,18 @@ parse_expect_version "$1"
 parse_version_from_log "$(swift --version)"
 
 if [[ $swift_version =~ $expected ]]; then
-  echo "Assert RegEx Match 'swift --version ($swift_version)' and $expected ("$1") success"
+  echo "Assert RegEx Match 'swift --version ($swift_version)' and $1 ("$expected") success"
 else
-  echo "Assert RegEx Match 'swift --version ($swift_version)' and $expected failed: $swift_version is not match to $expected ("$1")"
+  echo "Assert RegEx Match 'swift --version ($swift_version)' and $1 ("$expected") failed: $swift_version is not match to $expected"
   exit 1
 fi
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
   parse_version_from_log "$(xcrun swift --version)"
   if [[ $swift_version =~ $expected ]]; then
-    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $expected ("$1") success"
+    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $1 ("$expected") success"
   else
-    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $expected failed: $swift_version is not match to $expected ("$1")"
+    echo "Assert RegEx Match 'xcrun swift --version ($swift_version)' and $1 ("$expected") failed: $swift_version is not match to $expected"
     exit 1
   fi
 fi
